bf79e21781abf12caafa87b392ead9d211cfcaff
John Malapit <johncmalapit@gmail.com>
2022-09-12 17:35:49 -0400

    create GET, POST and DELETE requests

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
-const { Sequelix, QueryTypes, Sequelize } = require('sequelize');
+const { Model, Sequelize, DataTypes } = require('sequelize');
 const express = require('express');
 const app = express();
+
+app.use(express.json());
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
@@ -12,8 +14,67 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   },
 });
 
-app.get('/ping', async (req, res) => {
-  res.send('pong');
+class Blog extends Model {}
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    author: {
+      type: DataTypes.TEXT,
+    },
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: 'blog',
+  }
+);
+
+Blog.sync();
+
+app.get('/ping', async (req, res) => {});
+
+app.get('/api/blogs', async (req, res) => {
+  const blogs = await Blog.findAll();
+  res.json(blogs);
+});
+
+app.post('/api/blogs', async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body);
+    res.json(blog);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+
+app.delete('/api/blogs/:id', async (req, res) => {
+  try {
+    const blog = await Blog.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json(blog);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
 });
 
 const PORT = process.env.PORT || 3001;
