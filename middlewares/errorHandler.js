d544bdf9b220a39e7ea96e59675be3331d0a5ce8
John Malapit <johncmalapit@gmail.com>
2022-09-13 20:02:04 -0400

    implement username validation

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,13 +1,16 @@
 const errorHandler = (error, req, res, next) => {
-  console.error('error message:', error.message);
+  console.error('Error Handler:', error);
   if (error.name === 'SequelizeDatabaseError') {
     return res.status(400).send({ error: 'malformatted id' });
   }
   if (error.name === 'SequelizeValidationError') {
-    return res.status(400).send({ error: 'missing field' });
+    return res.status(400).send({ error: error.message });
   }
   if (error.name === 'TypeError') {
     return res.status(400).send({ error: 'null not found' });
+  }
+  if (error.name === 'SequelizeUniqueConstraintError') {
+    return res.status(400).send({ error: 'request outside of constraint' });
   }
   next(error);
 };
